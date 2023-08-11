fae6874ebd23e5081167377114e4808f0e20ea42
Thaliaraujo <thalia.araujo321@gmail.com>
2023-08-11 13:53:10 -0300

    commit

--- js/camera.js
+++ js/camera.js
@@ -4,6 +4,7 @@ const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
 const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
 
 let imagemURL = "";
 
@@ -24,4 +25,15 @@ botaoTirarFoto.addEventListener('click', function() {
 
     campoCamera.style.display = "none";
     mensagem.style.display = "block";
+});
+
+botaoEnviarFoto.addEventListener('click', () => {
+    const receberDados = localStorage.getItem('cadastro');
+    const converteRetorno = JSON.parse(receberDados);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = './abrir-conta-form-3.html';
 });
